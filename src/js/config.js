f8b156e0fb7a1a8d3295b30d555c9ce8fdcfe879
Giorgio <gaionifelappi@gmail.com>
2026-09-26 17:59:49 +0200

    1.2.5

--- src/js/config.js
+++ src/js/config.js
@@ -8,7 +8,7 @@ const BURRACO_CONFIG = {
   // ==========================================
   // VERSIONAMENTO E RETE
   // ==========================================
-  version: "1.2.4",
+  version: "1.2.5",
   remoteUrl: "https://burracopunti.giust.workers.dev/",
 
   // ==========================================
